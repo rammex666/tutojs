2ae21e8f93bd03708b8cffc21e479f951f8542fa
Rammex <romain.kaspar@gmail.com>
2022-06-03 15:06:24 +0200

    commit message

---
+++ index.js
@@ -0,0 +1,36 @@
+const {Client, Intents} = require('discord.js');
+const {token} = require("./config.json");
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+const prefixCmd = '!';
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", msg => {
+
+    // Si le message n'est pas préfixé ou qu'il vient d'un autre bot, nous l'ignorons
+    if(!msg.content.startsWith(prefixCmd) || msg.author.bot) return
+
+    // Si nous arrivons jusque ici, alors c'est une commande
+
+    // Nous convertissons la commande sous forme de tableau en prenant soin de retirer le préfixe
+    const args = msg.content.slice(prefixCmd.length).trim().split(/ +/);
+    // Extraction du premier élément de 'args', ce qui correspond à la commande
+    const command = args.shift().toLowerCase();
+
+    // À ce stade, args est un tableau ne contenant que les arguments étant donné que la commande a été extraite de celui-ci
+
+    // On se sert maintenant de la varibale 'command' pour le test
+    if (command === "ping") {
+        msg.reply("pong");
+    }
+
+});
+
+client.login(token);
